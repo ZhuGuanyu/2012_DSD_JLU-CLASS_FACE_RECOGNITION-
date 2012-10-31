3e8a4f0ead7bc954af70283f2880648548beeb4f
guichen <guiallanchen@gmail.com>
2012-10-31 15:38:17 +0800

    Welcome to China!

---
+++ src/java/Action/TestGit.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Action;
+
+/**
+ *
+ * @author Administrator
+ */
+public class TestGit {
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to China!");
+    }
+}
